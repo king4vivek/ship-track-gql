75d1811549bbebd776bc0183130c13dc3a6a12f9
king4vivek <39230646+king4vivek@users.noreply.github.com>
2025-01-23 15:52:40 +0530

    Root files

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { typeDefs } from "./src/types.js";
 import { resolvers } from "./src/resolvers.js";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { createServer } from 'http';
+import { expressMiddleware } from "@apollo/server/express4";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/use/ws";
+import express from 'express';
+import bodyParser from "body-parser";
 
-const apolloServer = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
+const port = 4300;
+const path = '/graphql';
 
-const server = await startStandaloneServer(apolloServer, { listen: { port: 4300 } });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-console.log(`🚀  Server ready at: ${server.url}`);
+const apolloServer = new ApolloServer({ schema: schema });
+await apolloServer.start();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(path, expressMiddleware(apolloServer));
+
+const httpServer = createServer(app);
+
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: path,
+});
+
+useServer({ schema, }, wsServer);
+
+httpServer.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}${path}`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:${port}${path}`);
+});
